07d4acc73a2049983e1e4ba81cd194b1b46ab4f7
Felipe Uribe <wfpaisa@gmail.com>
2025-06-17 11:32:13 -0500

    add host

---
+++ apps/dashboard/src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { NxWelcome } from './nx-welcome';
+import { Route } from '@angular/router';
+
+export const appRoutes: Route[] = [
+  {
+    path: '',
+    component: NxWelcome,
+  },
+];
---
+++ apps/dashboard/src/app/app.spec.ts
@@ -0,0 +1,40 @@
+import { fakeAsync, TestBed, tick } from '@angular/core/testing';
+import { App } from './app';
+import { NxWelcome } from './nx-welcome';
+import { Router, RouterModule } from '@angular/router';
+
+describe('App', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [
+        RouterModule.forRoot([{ path: '', component: NxWelcome }]),
+        App,
+        NxWelcome,
+      ],
+    }).compileComponents();
+  });
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(App);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have as title 'dashboard'`, () => {
+    const fixture = TestBed.createComponent(App);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('dashboard');
+  });
+
+  it('should render title', fakeAsync(() => {
+    const fixture = TestBed.createComponent(App);
+    const router = TestBed.inject(Router);
+    fixture.ngZone?.run(() => router.navigate(['']));
+    tick();
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      'Welcome dashboard'
+    );
+  }));
+});
